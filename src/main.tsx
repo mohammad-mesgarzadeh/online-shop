7a6604883868926e27849a37b59a31b852a68faa
Mohammad Mesgarzadeh <m_mesgarzadeh@yahoo.com>
2026-07-19 11:33:33 +0330

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
